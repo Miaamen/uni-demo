5a503d6d52a3804f989d2ff0cf14810d3da9ce04
Mia <42193533+Miaamen@users.noreply.github.com>
2022-09-22 20:17:28 +0800

    Add files via upload

---
+++ my-vue3-project/src/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <view class="content">
+        <comment-list class="list"
+            is-folded="true"
+            :comment-param="commentParam"
+        ></comment-list>
+    </view>
+</template>
+
+<script>
+    export default {
+        setup() {
+            return {
+                title: 'hello',
+                commentParam: {
+                    snid: 'BD20200202',
+                    //  snid: 'test_123',
+                    path: '/pages/comment/index?snid=BD20200202',
+                    title: '测试文章标题',
+                    content: '测试文章内容'
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
